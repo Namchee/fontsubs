cbfcfce479f12011d61666e158ad0a9ebf8438eb
Cristopher <cristophernamchee12@gmail.com>
2024-04-28 21:41:58 +0700

    fix(astro): handle special case for 404 page

--- packages/astro/src/index.ts
+++ packages/astro/src/index.ts
@@ -1,80 +1,84 @@
-import { defineIntegration } from 'astro-integration-kit';
+import { defineIntegration } from "astro-integration-kit";
 
-import { z } from 'astro/zod';
+import { z } from "astro/zod";
 
-import { execSync } from 'node:child_process';
-import { resolve } from 'node:path';
+import { execSync } from "node:child_process";
+import { resolve } from "node:path";
 
 export const subset = defineIntegration({
-  name: 'subset',
-  optionsSchema: z.object({
-    /**
-     * List of characters to be whitelisted outside of detected glyphs.
-     *
-     * @default `''`
-     */
-    whitelist: z.string().optional().default(''),
-    /**
-     * Attempt to optimize unused variable fonts features such as
-     * weights, slants, etc when its unused.
-     *
-     * This feature is experimental and might break your fonts.
-     *
-     * @default `false`
-     */
-    optimizeVariableFonts: z.boolean().optional().default(true),
-    /**
-     * Inline the optimized fonts directly in the @font-face declaration.
-     *
-     * @default `false`
-     */
-    inline: z.boolean().optional().default(false),
-    /**
-     * Analyze webfonts usage dynamically using headless browsers.
-     *
-     * Mainly used for on-demand renders.
-     *
-     * @default `false`
-     */
-    dynamic: z.boolean().optional().default(false),
-    /**
-     * Enable verbose output to stdout.
-     *
-     * @default `false`
-     */
-    debug: z.boolean().optional().default(false),
-  }).optional().default({}),
+  name: "subset",
+  optionsSchema: z
+    .object({
+      /**
+       * List of characters to be whitelisted outside of detected glyphs.
+       *
+       * @default `''`
+       */
+      whitelist: z.string().optional().default(""),
+      /**
+       * Attempt to optimize unused variable fonts features such as
+       * weights, slants, etc when its unused.
+       *
+       * This feature is experimental and might break your fonts.
+       *
+       * @default `false`
+       */
+      optimizeVariableFonts: z.boolean().optional().default(true),
+      /**
+       * Inline the optimized fonts directly in the @font-face declaration.
+       *
+       * @default `false`
+       */
+      inline: z.boolean().optional().default(false),
+      /**
+       * Analyze webfonts usage dynamically using headless browsers.
+       *
+       * Mainly used for on-demand renders.
+       *
+       * @default `false`
+       */
+      dynamic: z.boolean().optional().default(false),
+      /**
+       * Enable verbose output to stdout.
+       *
+       * @default `false`
+       */
+      debug: z.boolean().optional().default(false),
+    })
+    .optional()
+    .default({}),
   setup: ({ options }) => {
     return {
       hooks: {
-        'astro:build:done': ({ dir, pages }) => {
-          const flags = ['--in-place', '--no-fallbacks', '--debug'];
+        "astro:build:done": ({ dir, pages }) => {
+          const flags = ["--in-place", "--no-fallbacks", "--debug"];
           if (options?.whitelist) {
             flags.push(`--text=${options.whitelist}`);
           }
 
           if (options?.optimizeVariableFonts) {
-            flags.push('--instance');
+            flags.push("--instance");
           }
 
           if (options?.inline) {
-            flags.push('--inline-css');
+            flags.push("--inline-css");
           }
 
           if (options?.dynamic) {
-            flags.push('--dynamic');
+            flags.push("--dynamic");
           }
 
-          flags.push(options?.debug ? '' : '');
+          const input = pages.map(({ pathname }) =>
+            resolve(
+              dir.pathname,
+              pathname,
+              pathname.startsWith("404") ? "" : "index.html",
+            ),
+          );
 
-          const input = pages.map(({ pathname }) => resolve(dir.pathname, pathname, 'index.html'));
-
-          execSync(`subfont ${input.join(' ')} ${flags.join(' ')}`);
+          execSync(`subfont ${input.join(" ")} ${flags.join(" ")}`);
         },
       },
     };
   },
 });
-
-
-
